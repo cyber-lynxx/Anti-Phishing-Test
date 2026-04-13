23e999c141426deac47fc36a69db4f9473785de8
Vik <vikbit.2025@gmail.com>
2026-04-13 13:27:50 -0700

    Replace test route with dynamic page route

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ app = Flask(__name__, template_folder='.')
 def index():
     return render_template("index.html")
 
-@app.route("/test")
-def test():
-    return render_template("test.html")
+@app.route("/page/<name>")
+def route(name):
+    result = render_template(f"{name}.html")
+    return result
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
