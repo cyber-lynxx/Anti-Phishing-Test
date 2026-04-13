87e4165238567b0387ff5bf961cbac64d99a2f3c
Vik <vikbit.2025@gmail.com>
2026-04-13 13:21:36 -0700

    Remove index and dynamic route functions
    Removed index and dynamic route functions from app.py.

--- app.py
+++ app.py
@@ -4,10 +4,3 @@ api_key = os.getenv("API_KEY")
 
 app = Flask(__name__, template_folder='.')
 
-@app.route("/")
-def go_to_index():
-  return redirect("/index")
-
-@app.route("/<name>")
-def route(name):
-  return render_template(f"{name}.html")
