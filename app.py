a9a7960684a2e50dc5ca40abf7639ef73ea7f466
Vik <vikbit.2025@gmail.com>
2026-04-13 13:25:05 -0700

    Add index and test routes to Flask app

--- app.py
+++ app.py
@@ -4,3 +4,13 @@ api_key = os.getenv("API_KEY")
 
 app = Flask(__name__, template_folder='.')
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template("test.html")
+
+if __name__ == '__main__':
+    app.run()
