a8396acaacc8b9b3df30621ddc1451112bde3fe9
Vik <vikbit.2025@gmail.com>
2026-04-18 12:33:37 -0700

    Rename index function and redirect to index.html

--- app.py
+++ app.py
@@ -5,13 +5,15 @@ api_key = os.getenv("API_KEY")
 app = Flask(__name__, template_folder='.')
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def route_index():
+    return redirect("index.html")
 
 @app.route("/<name>")
 def route(name):
     result = render_template(f"{name}.html")
     return result
+
+
 
 if __name__ == "__main__":
     app.run()
