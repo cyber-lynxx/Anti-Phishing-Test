d5810d3922fd1e51affcd7ced8aac06510c13219
Vik <vikbit.2025@gmail.com>
2026-04-19 18:27:27 -0700

    Add event listeners for yes and no buttons

--- static/email.js
+++ static/email.js
@@ -1,25 +1,59 @@
-let excerpt1 = ""
-let excerpt2 = ""
-let excerpt3 = ""
-let excerpt4 = ""
-let excerpt5 = ""
+let excerpt1 = "";
+let excerpt2 = "";
+let excerpt3 = "";
+let excerpt4 = "";
+let excerpt5 = "";
 
+// The email
 fetch("/excerpt1")
   .then(excerpt => excerpt.text())
-  .then(str => excerpt1 = str)
+  .then(str => excerpt1 = str);
 
+// Explanation
 fetch("/excerpt2")
   .then(excerpt => excerpt.text())
-  .then(str => excerpt2 = str)
+  .then(str => excerpt2 = str);
 
+// Answer (YES/NO)
 fetch("/excerpt3")
   .then(excerpt => excerpt.text())
-  .then(str => excerpt3 = str)
+  .then(str => excerpt3 = str);
 
-fetch("/excerpt4)
+// Subject
+fetch("/excerpt4")
       .then(excerpt => excerpt.text())
-      .then(str => excerpt4 = str)
+      .then(str => excerpt4 = str);
 
-fetch("/excerpt5)
+// Email Address
+fetch("/excerpt5")
       .then(excerpt => excerpt.text())
-      .then(str => excerpt5 = str)
+      .then(str => excerpt5 = str);
+
+// "printing" the excerpts onto the HTML
+const subjectElement = document.getElementById("subject");
+subjectElement.textContent = excerpt4;
+
+const email_addressElement = document.getElementById("email_address");
+email_addressElement.textContent = excerpt5;
+
+const emailElement = document.getElementById("email");
+emailElement.textContent = excerpt1;
+
+// Checking the user's answer
+const yesButton = document.getElementById("yesbutton");
+yesButton.addEventListener("click", () => {
+  const result = yesButton.value === excerpt3.trim() ? "correct" : "incorrect";
+
+  sessionStorage.setItem("explanation", excerpt2);
+  
+  window.location.href = `results?answer=${result}`;
+});
+
+const noButton = document.getElementById("nobutton");
+noButton.addEventListener("click", () => {
+  const result = noButton.value === excerpt3.trim() ? "correct" : "incorrect";
+
+  sessionStorage.setItem("explanation", excerpt2);
+  
+  window.location.href = `results?answer=${result}`;
+});
