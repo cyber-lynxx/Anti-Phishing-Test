6af2a08acf028774d84576db500cacd882d4f3f7
Vik <vikbit.2025@gmail.com>
2026-05-03 15:15:26 -0700

    Fix indentation and add favicon route

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, redirect, url_for
 import os
-api_key = os.getenv("API_KEY")
 
+api_key = os.getenv("API_KEY")
 app = Flask(__name__)
 
 @app.route("/")
 def route_index():
-  return redirect("index")
+    return redirect("index")
+
+@app.route("/favicon.ico")
+def favicon():
+    return "", 204
+
+@app.route("/text")
+def get_text():
+    return "your email content here", 200, {"Content-Type": "text/plain"}
 
 @app.route("/<name>")
 def route(name):
     result = render_template(f"{name}.html")
     return result
-
-
 
 if __name__ == "__main__":
     app.run()
